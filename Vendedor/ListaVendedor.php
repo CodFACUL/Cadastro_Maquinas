81de2c9588a05b4894d133bc3c8bc99d537c88dc
CodFACUL <lucas.porfirio@unidavi.edu.br>
2021-05-21 19:44:54 -0300

    vendedor

---
+++ Vendedor/ListaVendedor.php
@@ -0,0 +1,31 @@
+<?php 
+
+require ('./Header.php')
+
+?>
+<table class="table  table-striped">
+    <thead class="bg-primary">
+        <tr>
+            <th>CNPJ</th>
+            <th>Vendedor</th>
+            <th>Clientes</th>
+            <th>Maquinas vendidas</th>
+            <th>Ações</th>
+        </tr>
+    </thead>
+    <tbody class="table-secondary text-dark">
+        <tr>
+            <td>XX. XXX. XXX/0001-XX</td>
+            <td>Daniel</td>
+            <td>1</td>
+            <td>3</td>
+            <td><button class="btn btn-primary">Editar</button ><button class="btn btn-danger">Excluir</button></td>
+        </tr>
+    </tbody>
+</table>
+
+<?php 
+
+require ('./Footer.php')
+
+?>
