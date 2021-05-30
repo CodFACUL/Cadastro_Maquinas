9c6953863182d8ea1acfea474effa991dafb4b37
CodFACUL <lucas.porfirio@unidavi.edu.br>
2021-05-29 23:08:28 -0300

    form lamina

--- Laminas/FormularioLamina.php
+++ Laminas/FormularioLamina.php
@@ -2,26 +2,49 @@
 <br>
 
 <form method="POST">
-    <div class="input-group mb-3">
-         <label class="input-group-text"for="cnpj">CNPJ</label>
-         <input type="text" class="form-control col-3" name="cnpj" value="<?php if(isset($obCliente->cnpj_cli)){echo $obCliente->cnpj_cli;}  ?>">
-    </div>
-    <div class="input-group mb-3">
-         <label class="input-group-text"for="cliente">Cliente</label>
-         <input type="text" class="form-control" name="cliente" value="<?php if(isset($obCliente->nome)){echo $obCliente->nome;}?>">
-    </div>
-    <div class="input-group mb-3">
-         <label class="input-group-text"for="qtd_maq">Quantidade de Maquinas</label>
-         <input type="text" class="form-control col-1" name="qtd_maq" value="<?php if(isset($obCliente->qtd_maq)){echo $obCliente->qtd_maq;}?>">
-    </div>
-    <div class="input-group mb-3">
-                 <label for="vendedor" class="input-group-text">Vendedor</label>
-                 <select name="vendedor" class="btn col-6 form-select">
-                    <option selected>Escolha...</option>
-                    <?php $imprime==''?$imprime='<option>Não há vendedores cadastrados</option>':$imprime;
-                    echo $imprime?>
-                 </select>
-            </div>
-    <input type="submit" class="btn btn-success mr-2"name="salvar" value="Salvar"><a class="btn btn-danger" href="./ListaCliente.php">Cancelar</a>
+<h5>Lâmina</h5>
+                <div class="text-dark bg-light">
+
+    <table class="table  table-striped">
+                <thead class="bg-secondary">
+                    <tr>
+                        <th>Afiação</th>
+                        <th>Diametro Interno</th>
+                        <th>Diametro Externo</th>
+                        <th>Quantidade</th>
+                    </tr>
+                </thead>
+                <tbody class="table-white text-dark">
+                    <tr>
+                        <td> <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" name="lamina" value="Lisa">
+                                    <label for="lisa"class="form-check-label">Lisa</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" name="lamina" value="Serrilhada">
+                                    <label for="lisa"class="form-check-label">Serrilhada</label>
+                                </div></td>
+                        <td> <div class="form-check">
+                                    <input type="radio" id="57" class="form-check-input" name="interno" value="57">
+                                    <label for="57"class="form-check-label">57mm</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" id="58"class="form-check-input" name="interno" value="58">
+                                    <label for="58"class="form-check-label">58mm</label>
+                                </div></td>
+                                <td> <div class="form-check">
+                                    <input type="radio" id="300" class="form-check-input" name="externo" value="300">
+                                    <label for="300"class="form-check-label">300mm</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" id="350"class="form-check-input" name="externo" value="350">
+                                    <label for="350"class="form-check-label">350mm</label>
+                                </div></td>
+                        <td><input type="text" value="1" name="qtdlamina" class="text-dark col-1"></td>
+                    </tr>
+                    
+                </tbody>
+</table>
+</div>
 
 </form>
